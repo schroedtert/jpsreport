2d62b4748cd82a3a97d261acd8b7e5b77273e384
schroedtert <t.schroedter@fz-juelich.de>
2021-05-04 19:37:05 +0200

    Move files to jpsreport

---
+++ cmake_modules/helper_functions.cmake
@@ -0,0 +1,34 @@
+function(print_var variable)
+    message(STATUS "${variable}=${${variable}}")
+endfunction()
+
+# There is a long outstanding issue with CMake where CMake does not support
+# relative paths in CMAKE_PREFIX_PATH but does not report anything. Many hours
+# have been wasted over this behavior. We are checking to save your this to
+# save everyone some headache. :)
+function(check_prefix_path)
+    if(CMAKE_PREFIX_PATH)
+        foreach(path ${CMAKE_PREFIX_PATH})
+            if(NOT IS_ABSOLUTE ${path})
+                message(FATAL_ERROR "CMake does not support relative paths for CMAKE_PREFIX_PATH! [${path}]")
+            endif()
+        endforeach()
+    endif()
+endfunction()
+
+function(find_python_library python_lib)
+    execute_process(
+            COMMAND
+            ${PYTHON_EXECUTABLE} "-c" "import ${python_lib}; print(${python_lib}.__version__)"
+            RESULT_VARIABLE status
+            OUTPUT_VARIABLE version
+            ERROR_QUIET
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    if(NOT status)
+        print_var(python_lib)
+        print_var(version)
+    else()
+        message(FATAL_ERROR "Did not find package ${python_lib}")
+    endif()
+endfunction()
